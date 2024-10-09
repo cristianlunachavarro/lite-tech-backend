44293c47d7e4000025a927b60e77442921b92ebd
Cristian Luna <cristianluna@Cristians-MBP-2.fibertel.com.ar>
2024-10-09 15:34:03 -0300

    add public dire

--- api/src/main.ts
+++ api/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
