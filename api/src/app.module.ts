e737ea19293901779e94f24fb9b14a8a621f0e22
Cristian Luna <cristianluna@Cristians-MBP-2.fibertel.com.ar>
2024-10-09 17:04:01 -0300

    changes on vercel json change connect to mongo

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PostModule } from './posts/posts.module'; 
+import { PostModule } from './posts/posts.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot(
-      {
-        rootPath: join(__dirname, '..', 'uploads'),
-        serveRoot: '/uploads/',
-      },
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads/',
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPASSWORD}@lite-tech.q1tro.mongodb.net/?retryWrites=true&w=majority&appName=lite-tech`,
     ),
-    MongooseModule.forRoot('mongodb://localhost:27017/litebox_db'), 
     PostModule,
   ],
 })
